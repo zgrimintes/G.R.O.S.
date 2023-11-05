90bb224132ee6df1395b8a308e6bce896c451920
Tibirean Petru <peptib07@gmail.com>
2023-11-05 12:59:41 +0200

    Damage struct changes

--- Damage.cs
+++ Damage.cs
@@ -1,8 +1,26 @@
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public struct Damage
 {
     public Vector3 origin;
-    public int dmg;
+    public float dmg;
+    public float dmgBonus;
     public float pushForce;
+
+    public void setBonus(float dmgBonus)
+    {
+        if (dmgBonus < 1)
+        {
+            this.dmgBonus = 1;
+        }
+        else if (dmgBonus == 2)
+        {
+            this.dmgBonus = Random.Range(2f, 4f);
+        }
+        else
+        {
+            this.dmgBonus = Random.Range(1f, 2f);
+        }
+    }
 };
