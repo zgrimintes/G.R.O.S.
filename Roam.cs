e293b20cafe95a08129097b7a5c5f5c1a24b5403
Tibirean Petru <peptib07@gmail.com>
2023-11-06 22:25:46 +0200

    Add checkig if the position the sheep wants to go to is valid

--- Roam.cs
+++ Roam.cs
@@ -1,16 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.IO.LowLevel.Unsafe;
 using UnityEngine;
 
 public class Roam : MonoBehaviour
 {
     private float timeToStay = 0;
-    private float lastStop;
+    public float lastStop;
 
     private Vector3 dirToMove;
+    BoxCollider2D m_Colider; //Reference to the component BoxCollider2D of th ground sprite
 
-    void Update()
+    private void Start()
     {
+        GameObject gorund = GameObject.FindWithTag("Ground");
+        m_Colider = gorund.GetComponent<BoxCollider2D>(); //Set the variable
+    }
+
+    private void Update()
+    {
+
         if (Time.time - lastStop > timeToStay)
         {
             lastStop = Time.time;
@@ -22,10 +31,13 @@ public class Roam : MonoBehaviour
 
     private void chooseDirection()
     {
-        Vector3 rot = new Vector3(Random.Range(-5f, 5f), Random.Range(-5f, 5f), 0);
-        transform.up = rot - transform.position;
+        do
+        {
+            Vector3 rot = new Vector3(Random.Range(-5f, 5f), Random.Range(-5f, 5f), 0);
+            transform.up = rot - transform.position;
 
-        dirToMove = transform.position - rot;
+            dirToMove = transform.position - rot;
+        } while (canGo(dirToMove)); //Repeats until you find a valid spot on the map
     }
 
     IEnumerator moveToPos(Vector3 targetPosition, float duration)
@@ -40,4 +52,12 @@ public class Roam : MonoBehaviour
 
         yield return null;
     }
+
+    private bool canGo(Vector3 destination) //This verifies if the position the goats are trying to go to is valid
+    {
+        if (m_Colider.bounds.Contains(destination))
+            return false;
+        return true;
+    }
+
 }
