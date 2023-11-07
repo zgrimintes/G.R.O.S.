abcb28e83a6c401dd404572afdf48c8cd975cc92
Tibirean Petru <peptib07@gmail.com>
2023-11-07 22:35:33 +0200

    Unfinished fence collision mechaninc

--- Walking.cs
+++ Walking.cs
@@ -2,26 +2,37 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Walking : MonoBehaviour
+public class Walking : Collidable
 {
-    public  Vector3 direction;
+    public Vector3 direction;
     public float speed;
     public bool canLook = true;
 
     // Update is called once per frame
-    void Update()
+    protected override void Update()
     {
+        base.Update();
+
         if (canLook)
         {
             Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            transform.up = mousePos - new Vector2(transform.position.x, transform.position.y); //The character is rotating after de cursor
- 
+            transform.up = mousePos - new Vector2(transform.position.x, transform.position.y);
+
             float horizontal = Input.GetAxisRaw("Horizontal");
             float vertical = Input.GetAxisRaw("Vertical");
 
-            direction = ((Vector2)transform.position - mousePos).normalized; //Direction for the charge animation
+            direction = ((Vector2)transform.position - mousePos).normalized;
             gameObject.transform.position += new Vector3(horizontal, vertical, 0) * Time.deltaTime * speed;
         }
 
+    }
+
+    protected override void OnCollide(Collider2D coll)
+    {
+        if (coll.tag == "Fence")
+        {
+            //transform.position = Vector3.Reflect(gameObject.transform.position ,Vector3.right);
+            //Debug.Log("fence!");
+        }
     }
 }
