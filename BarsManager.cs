90c32214d430e3db8e8cd12570a5785ab0b0bd04
Tibirean Petru <92109049+zgrimintes@users.noreply.github.com>
2024-03-23 19:26:08 +0200

    Update BarsManager.cs

--- BarsManager.cs
+++ BarsManager.cs
@@ -1,1 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
 
+public class BarsManager : MonoBehaviour
+{
+    public Slider healthBarSlider;
+
+    float posCos, posSin;
+
+    public void setMaxValue(float value)
+    {
+        healthBarSlider.maxValue = value;
+        healthBarSlider.value = value;
+    }
+
+    public void setValue(float value)
+    {
+        healthBarSlider.value = value;
+    }
+
+    private void getParentRotation()
+    {
+        Quaternion parentRot = gameObject.GetComponentInParent<Transform>().rotation = Quaternion.identity;
+
+        posCos = Mathf.Cos(parentRot.z);
+        posSin = Mathf.Sin(parentRot.z);
+
+        Debug.Log(parentRot.z);
+    }
+
+    private void Update()
+    {
+        getParentRotation();
+
+        transform.rotation = Quaternion.identity;
+        //transform.localPosition = new Vector3(0, -posSin, 0);
+        Debug.Log(posSin);
+    }
+}
