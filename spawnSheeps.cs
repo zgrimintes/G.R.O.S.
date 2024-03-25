289263e593e22a906db991f94025997e7574b579
Tibirean Petru <peptib07@gmail.com>
2024-03-25 20:29:35 +0200

    Added a sheep limit

--- spawnSheeps.cs
+++ spawnSheeps.cs
@@ -10,6 +10,7 @@ public class spawnSheeps : MonoBehaviour
     public float Cooldown;
 
     private float NextSheep = 2;
+    public int max_sheeps = 7;
 
     // Start is called before the first frame update
     void Start()
@@ -20,8 +21,10 @@ public class spawnSheeps : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Time.time > NextSheep)
+        if (Time.time > NextSheep && GameManager.sheep_count < max_sheeps)
         {
+            GameManager.instance.sheep_mod(1); //Signal to the manager the addition of a sheep
+
             NextSheep = Time.time + Cooldown;
             GameObject newSheep = Instantiate(sheeps, sheepSpawner.transform.position, Quaternion.identity); //I'm creating a new GameObject so i can change the script on it
             GameObject newSheep_HP = Instantiate(hb, newSheep.transform.position, Quaternion.identity); //I'm creating a HP bar too
